2343b8bb8e806aa01d7762b0e73d9a151679b1f7
GeorgePowhattan <martinek.ondrej@gmail.com>
2021-06-21 20:39:20 +0200

    update

---
+++ test.py
@@ -0,0 +1,7 @@
+booksDic = {1: 'LOTR', 2: 'Witcher'}
+
+books = {"book": [{"id": 1, "author": "Sapkowski", "name": "Witcher"},
+            {"id": 2, "author": "Tolkien", "name": "LOTR"}]}
+
+# print (books["book"][0]["id"])
+
